5d569ea5ed5253a809b53eef0aa0f95d894f24ef
Andrei Holub <holubgetrekt@gmail.com>
2022-04-02 19:47:53 +0200

    Bug fixes

--- src/main/java/com/example/demo/Controllers/HomeController.java
+++ src/main/java/com/example/demo/Controllers/HomeController.java
@@ -2,12 +2,12 @@ package com.example.demo.Controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
 
-    @GetMapping(("/home"))
+    @GetMapping("/home")
     public String home(Model model){
         return "home";
     }
--- src/main/java/com/example/demo/Controllers/TaskController.java
+++ src/main/java/com/example/demo/Controllers/TaskController.java
@@ -5,14 +5,17 @@ import com.example.demo.Services.TaskService;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
+
+@RestController
 public class TaskController {
-    private TaskService taskService;
+    public final TaskService taskService;
     public TaskController(TaskService taskService){
         this.taskService = taskService;
     }
 
-    @PostMapping("/list{id}/createTask")
+    @PostMapping("/list/{id}/createTask")
     public long createBook(@PathVariable Long id, @RequestBody Task TaskDto) {
         return TaskService.createTask(id, TaskDto);
     }
